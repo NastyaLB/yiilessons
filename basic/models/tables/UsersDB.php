078ca18e8102e4cee369e5cd19d18f0e1a15c732
Nast <blackstonesword@mail.ru>
2020-01-21 11:02:03 +0400

    Класс таблицы юзеров

---
+++ basic/models/tables/UsersDB.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "usersDB".
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $name
+ * @property string $password
+ * @property string creator_id
+ */
+class UsersDB extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'usersDB';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'name', 'password'], 'required'],
+            [['login', 'name', 'password'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'name' => 'Name',
+            'password' => 'Password',
+        ];
+    }
+    
+}

fab1b3b10bdc714b827cf3d5d756e5bf81b9bf4a
Nast <blackstonesword@mail.ru>
2020-02-04 11:10:09 +0400

    ничего не поменялось

--- basic/widgets/views/my-task-layout.php
+++ basic/widgets/views/my-task-layout.php
@@ -18,10 +18,15 @@ display:block; width:372px; box-shadow: 1px 1px 5px gray; padding: 0 5px; positi
 
 .st0 { background-color: #a94442; } 
 
+.stN { background-color: #bbb; } 
+
 .taskinwidget { 
-width:100%; height: 100%; background-color:#fff; padding:5px; };</style>
-<style> .taskinwidget > h4 { color:#333; margin:0 0 5px; padding:0; }
+width:100%; height: 100%; background-color:#fff; padding:5px; }
+
+.taskinwidget > h4 { color:#333; margin:0 0 5px; padding:0; }
 
 .taskinwidget > p { font-size:13px; color:#333; margin:0; padding:0; height:75px; overflow: hidden; }
+
+.stN p { height:auto; }
 
 .gogolink { display:block; width:100%; height: 100%; position:absolute; bottom:0; left:0 } </style>";
